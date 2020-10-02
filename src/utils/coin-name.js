456769296ad2cd11d87aaaa6171e41014db9edbd
lafong <zynapse.tech@gmail.com>
2020-10-02 21:44:44 +0700

    Update coin icons

--- src/utils/coin-name.js
+++ src/utils/coin-name.js
@@ -3,27 +3,27 @@ export function cryptoName(){
     return {
         BTC : {
             name : 'BTC',
-            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/128x128/1.png'
+            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/64x64/1.png'
         },
         ETH : {
             name : 'ETH',
-            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/128x128/1027.png'
+            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png'
         },
         SRM : {
             name : 'SRM',
-            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/128x128/6187.png'
+            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/64x64/6187.png'
         },
         MSRM : {
             name : 'MSRM',
-            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/128x128/6187.png'
+            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/64x64/6187.png'
         },
         SUSHI : {
             name : 'SUSHI',
-            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/128x128/6758.png'
+            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/64x64/6758.png'
         },
         LINK : {
             name : 'LINK',
-            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/128x128/1975.png'
+            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/64x64/1975.png'
         },
         YFI : {
             name : 'YFI',
@@ -31,57 +31,60 @@ export function cryptoName(){
         },
         ALEPH : {
             name : 'ALEPH',
-            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/128x128/5821.png'
+            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/64x64/5821.png'
         },
         CREAM : {
             name : 'CREAM',
-            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/128x128/6193.png'
+            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/64x64/6193.png'
         },
         FRONT : {
             name : 'FRONT',
-            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/128x128/5893.png'
+            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/64x64/5893.png'
         },
         FTT : {
             name : 'FTT',
-            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/128x128/4195.png'
+            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/64x64/4195.png'
         },
         HGET : {
             name : 'HGET',
-            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/128x128/6949.png'
+            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/64x64/6949.png'
         },
         HNT : {
             name : 'HNT',
-            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/128x128/5665.png'
+            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/64x64/5665.png'
         },
         SOL : {
             name : 'SOL',
-            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/128x128/5426.png'
+            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/64x64/5426.png'
         },
         SXP : {
             name : 'SXP',
-            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/128x128/4279.png'
+            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/64x64/4279.png'
         },
         UBXT : {
             name : 'UBXT',
-            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/128x128/7040.png'
+            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/64x64/7040.png'
         },
         USDT : {
             name : 'USDT',
-            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/128x128/825.png'
+            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/64x64/825.png'
         },
         USDC : {
             name : 'USDC',
-            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/128x128/3408.png'
+            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png'
         },
         AKRO : {
             name : 'AKRO',
-            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/128x128/4134.png'
+            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/64x64/4134.png'
         },
-        default:{
-            name : '',
-            iconUrl : ''
+        UNI : {
+            name : 'UNI',
+            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/64x64/7083.png'
+        },
+        HXRO : {
+            name : 'HXRO',
+            iconUrl : 'https://s2.coinmarketcap.com/static/img/coins/64x64/3748.png'
         }
-
     }
   
 };
